30168d1b7f7de86e9b3b4bfc37c48f88e7a55b6e
tatsuma.kato <ec2-user@ip-172-31-22-12.us-east-2.compute.internal>
2018-06-28 05:47:50 +0000

    change title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Kemkow
                 </div>
 
                 <div class="links">
